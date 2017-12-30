8f87fcc5f4d8d3f98159788a1fcb8c2759d0d531
Mquinn <matt@mquinn.co.uk>
2017-12-30 18:02:14 +0000

    Added jquery/parsley for validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,21 +2,27 @@
 
 @section('title', '| Create New Post')
 
+@section('header_scripts')
+
+    @include('partials._jquery_parsley')
+
+@endsection
+
 @section('content')
 
     <div class="row">
         <div class="col-md-8 offset-md-2">
             <h1>Create New Post</h1>
             <hr>
-            <form method="POST" action="{{ route('post.store') }}">
+            <form method="POST" data-parsley-validate action="{{ route('post.store') }}">
                 {{ csrf_field() }}
                 <div class="form-group">
                     <label name="title">Title:</label>
-                    <input id="title" name="title" class="form-control">
+                    <input id="title" name="title" data-parsley-required class="form-control" data-parsley-trigger="change" data-parsley-maxlength="255">
                 </div>
                 <div class="form-group">
                     <label name="body">Post Body:</label>
-                    <textarea id="body" name="body" rows="10" class="form-control"></textarea>
+                    <textarea id="body" name="body" rows="10" data-parsley-required class="form-control"></textarea>
                 </div>
                 <input type="submit" value="Create Post" class="btn btn-success btn-lg btn-block">
                 <input type="hidden" name="_token" value="{{ Session::token() }}">
