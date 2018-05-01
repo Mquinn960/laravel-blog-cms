d3617ae7760896f865d6c21292da447a6098790e
Mquinn <matt@mquinn.co.uk>
2018-05-01 19:23:05 +0100

    housekeeping on controllers

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.zoho.com'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'matt@mquinn.co.uk'),
-        'name' => env('MAIL_FROM_NAME', 'Mquinn.co.uk Website'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('matt@mquinn.co.uk'),
+    'username' => env('MAIL_USERNAME'),
 
-    'password' => env('zohocarebear9!'),
+    'password' => env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
